8dc5e6a915ebbf910be7d5c36fe0138b6d128d42
Joel <joepotential@gmail.com>
2022-08-03 14:51:20 +0100

    section template done

---
+++ app.js
@@ -0,0 +1,5 @@
+//query select timeframe buttons
+//query select sections
+//add active class to make them active
+//add click event listener to the buttons and when a particular timeframe is active, 
+//map and return template of the particular part
